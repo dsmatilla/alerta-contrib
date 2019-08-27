5bac86a6d0a5a4611da8c4e767f442280826757e
Daniel Matilla <d.suarez@sofcloudit.com>
2019-08-27 15:02:22 +0200

    Adding missing fields

--- webhooks/monitis/alerta_monitis.py
+++ webhooks/monitis/alerta_monitis.py
@@ -20,17 +20,21 @@ class MonitisWebhook(WebhookBase):
         if context['alertType'] == 'RECOVERY':
             severity = 'ok'
 
+        create_time = parse_date(context['timestamp'])
+
         return Alert(
             resource=context['url'],
-            type=context['type'],
             event=context['name'].context['adddata'],
             environment=environment,
             severity=severity,
             service=[context['url']],
             group=context['group'],
+            value='',
             text=context['alertId'],
             tags=[context['type']],
             attributes={},
             origin='Monitis',
+            type=context['type'],
+            create_time=create_time,
             raw_data=json.dumps(payload, indent=4)
         )
